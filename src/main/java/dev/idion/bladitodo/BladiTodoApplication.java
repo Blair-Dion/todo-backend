cddff3d8ef8cfd6cb6ca6ac36e9787c95645f3a0
Dion <ksundong@gmail.com>
2020-11-29 11:34:39 +0900

    [#3][env]spring project 초기화:
    Spring Boot 2.4.0
    Web MVC
    JPA
    Lombok
    H2
    MySQL
    REST Docs

---
+++ src/main/java/dev/idion/bladitodo/BladiTodoApplication.java
@@ -0,0 +1,13 @@
+package dev.idion.bladitodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BladiTodoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BladiTodoApplication.class, args);
+  }
+
+}
