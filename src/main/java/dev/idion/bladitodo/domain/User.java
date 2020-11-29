84a4a3f7ff1cd1e0e9a60bf3943f8f16c1f5664a
Dion <ksundong@gmail.com>
2020-11-29 12:38:23 +0900

    [#4][feat]User 클래스 정의

--- src/main/java/dev/idion/bladitodo/domain/User.java
+++ src/main/java/dev/idion/bladitodo/domain/User.java
@@ -1,5 +1,17 @@
 package dev.idion.bladitodo.domain;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+
+@Getter
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class User {
 
+  private Long id;
+  private String profileImageUrl;
+  private String userId;
+  private String userName;
+  private String userNickname;
+  private String githubToken;
 }
