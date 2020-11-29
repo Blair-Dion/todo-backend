d499be33b4ca10a58a2a141d561173a47167355e
Dion <ksundong@gmail.com>
2020-11-29 14:02:38 +0900

    [#4][refactor]BoardId도 반환할 수 있도록 설정

--- src/main/java/dev/idion/bladitodo/domain/http/BoardWithListIdResponse.java
+++ src/main/java/dev/idion/bladitodo/domain/http/BoardWithListIdResponse.java
@@ -11,17 +11,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class BoardWithListIdResponse {
 
+  private Long boardId;
   private String name;
   private java.util.List<Long> listIds;
 
   @Builder(setterPrefix = "with")
-  private BoardWithListIdResponse(String name, java.util.List<Long> listIds) {
+  private BoardWithListIdResponse(Long boardId, String name, java.util.List<Long> listIds) {
+    this.boardId = boardId;
     this.name = name;
     this.listIds = listIds;
   }
 
   public static BoardWithListIdResponse from(Board board) {
     return BoardWithListIdResponse.builder()
+        .withBoardId(board.getId())
         .withName(board.getName())
         .withListIds(board.getLists().stream()
             .map(List::getId)
