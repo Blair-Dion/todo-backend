caac94db86a8abac8d58cb185186e61c41c3fcb3
Dion <ksundong@gmail.com>
2020-11-29 12:39:28 +0900

    [#4][feat]Board 클래스 정의

--- src/main/java/dev/idion/bladitodo/domain/Board.java
+++ src/main/java/dev/idion/bladitodo/domain/Board.java
@@ -1,6 +1,13 @@
 package dev.idion.bladitodo.domain;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+
+@Getter
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class Board {
 
-
+  private Long id;
+  private String name;
 }
