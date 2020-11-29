d1af25b323c62d65e50186813175bdc46854a295
Dion <ksundong@gmail.com>
2020-11-29 15:15:29 +0900

    [#4][refactor]필요없는 필드 제거

--- src/main/java/dev/idion/bladitodo/domain/User.java
+++ src/main/java/dev/idion/bladitodo/domain/User.java
@@ -11,17 +11,15 @@ public class User {
   private Long id;
   private String profileImageUrl;
   private String userId;
-  private String userName;
   private String userNickname;
   private String githubToken;
 
   @Builder(setterPrefix = "with")
-  private User(Long id, String profileImageUrl, String userId, String userName,
-      String userNickname, String githubToken) {
+  private User(Long id, String profileImageUrl, String userId, String userNickname,
+      String githubToken) {
     this.id = id;
     this.profileImageUrl = profileImageUrl;
     this.userId = userId;
-    this.userName = userName;
     this.userNickname = userNickname;
     this.githubToken = githubToken;
   }
