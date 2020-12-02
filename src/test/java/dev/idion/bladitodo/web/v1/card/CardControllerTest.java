25040f9bb6f8370e56171671b277d3526fe961e3
Dion <ksundong@gmail.com>
2020-12-02 21:30:02 +0900

    [#22][test]카드 수정 API 테스트 코드 작성

--- src/test/java/dev/idion/bladitodo/web/v1/card/CardControllerTest.java
+++ src/test/java/dev/idion/bladitodo/web/v1/card/CardControllerTest.java
@@ -6,6 +6,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
@@ -88,6 +89,64 @@ class CardControllerTest {
                 fieldWithPath("id").description("카드의 DB id").type(JsonFieldType.NUMBER),
                 fieldWithPath("title").description("카드의 제목").type(JsonFieldType.STRING),
                 fieldWithPath("contents").description("카드의 내용").type(JsonFieldType.STRING),
+                fieldWithPath("user_id").description("카드를 생성한 user id").type(JsonFieldType.STRING),
+                fieldWithPath("list_id").description("카드가 속한 List의 DB id")
+                    .type(JsonFieldType.NUMBER),
+                fieldWithPath("archived").description("카드가 보관처리 되었는지 여부")
+                    .type(JsonFieldType.BOOLEAN),
+                fieldWithPath("archived_datetime").description("카드 보관처리 일자").optional()
+                    .type(JsonFieldType.STRING)
+            )
+        ));
+  }
+
+  @Test
+  void Card_수정_API_테스트() throws Exception {
+    //given
+    long listId = 1L;
+    long cardId = 4L;
+    String title = "수정한 제목";
+    String contents = "수정한 내용";
+    String userId = "ksundong";
+    CardRequest cardRequest = new CardRequest();
+    cardRequest.setTitle(title);
+    cardRequest.setContents(contents);
+    // CardDTO
+    CardDTO cardDTO = CardDTO.builder()
+        .withId(cardId)
+        .withTitle(title)
+        .withContents(contents)
+        .withUserId(userId)
+        .withListId(listId)
+        .build();
+
+    //when
+    when(cardService.updateCard(eq(listId), eq(cardId), any(CardRequest.class)))
+        .thenReturn(cardDTO);
+
+    //then
+    MockHttpServletRequestBuilder requestBuilder =
+        put(PRE_URI + "/list/{list_id}/card/{card_id}", listId, cardId)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(asJsonString(cardRequest));
+    mockMvc.perform(requestBuilder)
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andDo(document("{class-name}/{method-name}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint()),
+            pathParameters(
+                parameterWithName("list_id").description("수정할 카드의 list DB id"),
+                parameterWithName("card_id").description("수정할 카드의 DB id")
+            ),
+            requestFields(
+                fieldWithPath("title").description("수정할 제목").type(JsonFieldType.STRING),
+                fieldWithPath("contents").description("수정할 내용").type(JsonFieldType.STRING)
+            ),
+            responseFields(
+                fieldWithPath("id").description("카드의 DB id").type(JsonFieldType.NUMBER),
+                fieldWithPath("title").description("카드의 수정된 제목").type(JsonFieldType.STRING),
+                fieldWithPath("contents").description("카드의 수정된 내용").type(JsonFieldType.STRING),
                 fieldWithPath("user_id").description("카드를 생성한 user id").type(JsonFieldType.STRING),
                 fieldWithPath("list_id").description("카드가 속한 List의 DB id")
                     .type(JsonFieldType.NUMBER),
