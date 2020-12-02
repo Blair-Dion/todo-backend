575bd0a7297b82d3689fcc047ba80f19886c304b
Dion <ksundong@gmail.com>
2020-12-02 11:20:09 +0900

    [#21][refactor]불필요한 어노테이션 제거

--- src/test/java/dev/idion/bladitodo/domain/board/UserRepositoryTest.java
+++ src/test/java/dev/idion/bladitodo/domain/board/UserRepositoryTest.java
@@ -5,14 +5,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import dev.idion.bladitodo.domain.user.User;
 import dev.idion.bladitodo.domain.user.UserRepository;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 class UserRepositoryTest {
