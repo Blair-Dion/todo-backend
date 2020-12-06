9154a030e0b0c21e14232dfe34f3cd870bff23e3
Dion <ksundong@gmail.com>
2020-12-06 13:34:21 +0900

    [#46][test]카드 이동 API 테스트 코드 작성

--- src/test/java/dev/idion/bladitodo/web/v1/card/CardControllerTest.java
+++ src/test/java/dev/idion/bladitodo/web/v1/card/CardControllerTest.java
@@ -24,6 +24,7 @@ import dev.idion.bladitodo.service.card.CardService;
 import dev.idion.bladitodo.web.dto.CardDTO;
 import dev.idion.bladitodo.web.dto.DTOContainer;
 import dev.idion.bladitodo.web.dto.LogDTO;
+import dev.idion.bladitodo.web.v1.card.request.CardMoveRequest;
 import dev.idion.bladitodo.web.v1.card.request.CardRequest;
 import java.util.StringJoiner;
 import org.junit.jupiter.api.DisplayName;
@@ -209,6 +210,99 @@ class CardControllerTest {
                 fieldWithPath("result.id").description("카드의 DB id").type(JsonFieldType.NUMBER),
                 fieldWithPath("result.title").description("카드의 수정된 제목").type(JsonFieldType.STRING),
                 fieldWithPath("result.contents").description("카드의 수정된 내용")
+                    .type(JsonFieldType.STRING),
+                fieldWithPath("result.user_id").description("카드를 생성한 user id")
+                    .type(JsonFieldType.STRING),
+                fieldWithPath("result.profile_image_url").description("카드 소유자의 프로필 이미지")
+                    .type(JsonFieldType.STRING),
+                fieldWithPath("result.list_id").description("카드가 속한 List의 DB id")
+                    .type(JsonFieldType.NUMBER),
+                fieldWithPath("result.archived").description("카드가 보관처리 되었는지 여부")
+                    .type(JsonFieldType.BOOLEAN),
+                fieldWithPath("result.archived_datetime").description("카드 보관처리 일자").optional()
+                    .type(JsonFieldType.STRING),
+                fieldWithPath("log.id").description("로그의 DB id").type(JsonFieldType.NUMBER),
+                typeField("log.type"),
+                fieldWithPath("log.before_title").description("이전 제목").type(JsonFieldType.STRING)
+                    .optional(),
+                fieldWithPath("log.after_title").description("이후 제목").type(JsonFieldType.STRING)
+                    .optional(),
+                fieldWithPath("log.before_contents").description("이전 내용").type(JsonFieldType.STRING)
+                    .optional(),
+                fieldWithPath("log.after_contents").description("이후 내용").type(JsonFieldType.STRING)
+                    .optional(),
+                fieldWithPath("log.from_list_id").description("이전 리스트의 DB id")
+                    .type(JsonFieldType.NUMBER).optional(),
+                fieldWithPath("log.to_list_id").description("이후 리스트의 DB id")
+                    .type(JsonFieldType.NUMBER).optional()
+            )
+        ));
+  }
+
+  @Test
+  void Card_이동_API_테스트() throws Exception {
+    //given
+    long boardId = 1L;
+    long listId = 1L;
+    long destinationListId = 3L;
+    long cardId = 3L;
+    String title = "제목";
+    String contents = "내용";
+    String userId = "ksundong";
+    String profileImageUrl = "https://avatars0.githubusercontent.com/u/38597469?s=88&u=4dec19ec378bfb64c9b4a00c4d63e7805dac9c6c&v=4";
+    // CardMoveRequest
+    CardMoveRequest cardMoveRequest = new CardMoveRequest();
+    cardMoveRequest.setDestinationListId(destinationListId);
+    // CardDTO
+    CardDTO cardDTO = CardDTO.builder()
+        .withId(cardId)
+        .withTitle(title)
+        .withContents(contents)
+        .withUserId(userId)
+        .withProfileImageUrl(profileImageUrl)
+        .withListId(destinationListId)
+        .build();
+    // LogDTO
+    LogDTO logDTO = LogDTO.builder()
+        .withId(1L)
+        .withType(LogType.CARD_MOVE)
+        .withFromListId(listId)
+        .withToListId(destinationListId)
+        .build();
+    // DTOContainer
+    DTOContainer container = new DTOContainer(cardDTO, logDTO);
+
+    //when
+    when(cardService.moveCard(eq(boardId), eq(listId), eq(cardId), any(CardMoveRequest.class)))
+        .thenReturn(container);
+
+    //then
+    MockHttpServletRequestBuilder requestBuilder =
+        put(PRE_URI + "/board/{board_id}/list/{list_id}/card/{card_id}/move", boardId, listId,
+            cardId)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(asJsonString(cardMoveRequest));
+    mockMvc.perform(requestBuilder)
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andDo(document("{class-name}/{method-name}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint()),
+            pathParameters(
+                parameterWithName("board_id").description("카드가 속한 보드의 DB id"),
+                parameterWithName("list_id").description("카드가 속한 리스트 DB id"),
+                parameterWithName("card_id").description("이동할 카드의 DB id")
+            ),
+            requestFields(
+                fieldWithPath("destination_list_id").description("이동할 리스트의 DB id")
+                    .type(JsonFieldType.NUMBER)
+            ),
+            responseFields(
+                fieldWithPath("result_type").description("result의 DTO 타입")
+                    .type(JsonFieldType.STRING),
+                fieldWithPath("result.id").description("카드의 DB id").type(JsonFieldType.NUMBER),
+                fieldWithPath("result.title").description("카드의 제목").type(JsonFieldType.STRING),
+                fieldWithPath("result.contents").description("카드의 내용")
                     .type(JsonFieldType.STRING),
                 fieldWithPath("result.user_id").description("카드를 생성한 user id")
                     .type(JsonFieldType.STRING),
