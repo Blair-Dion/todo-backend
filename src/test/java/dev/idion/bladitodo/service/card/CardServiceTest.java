34cfb6923129967733fe7b86bb8221a29f138284
Dion <ksundong@gmail.com>
2020-12-26 17:20:19 +0900

    [#74][test]카드 생성 테스트 코드 개선

--- src/test/java/dev/idion/bladitodo/service/card/CardServiceTest.java
+++ src/test/java/dev/idion/bladitodo/service/card/CardServiceTest.java
@@ -2,6 +2,7 @@ package dev.idion.bladitodo.service.card;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 
 import dev.idion.bladitodo.domain.board.Board;
@@ -27,6 +28,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class CardServiceTest {
 
+  public static final String RESULT_TYPE_CARD = "card";
+
+  final long existBoardId = 1L;
+  final long existListId = 1L;
+  final long existUserId = 1L;
+  final String title = "카드 제목";
+  final String contents = "카드 내용";
+
   @InjectMocks
   CardService cardService;
 
@@ -45,34 +54,40 @@ class CardServiceTest {
   @Mock
   LogRepository logRepository;
 
+  Board board;
+  User user;
+  List list;
+  CardRequest request;
+  Card card;
+
   @BeforeEach
   void setUp() {
+    board = Board.builder().build();
+    user = User.builder().build();
+    list = List.builder().build();
+    list.setBoard(board);
+
+    request = new CardRequest();
+    request.setTitle(title);
+    request.setContents(contents);
+
+    card = request.toEntity();
+    card.setList(list);
+    card.setUser(user);
   }
 
   @Test
   @DisplayName("card 생성 성공 테스트")
   void createCardTest() {
-    Board board = Board.builder().build();
-    User user = User.builder().build();
-    List list = List.builder().build();
-    list.setBoard(board);
-
-    CardRequest request = new CardRequest();
-    request.setTitle("카드 제목");
-    request.setContents("카드 내용");
-
-    Card card = request.toEntity();
-
-    given(boardRepository.findByBoardId(1L)).willReturn(Optional.of(board));
-    given(listRepository.findById(1L)).willReturn(Optional.of(list));
-    given(userRepository.findById(1L)).willReturn(Optional.of(user));
+    given(boardRepository.findByBoardId(eq(existBoardId))).willReturn(Optional.of(board));
+    given(listRepository.findById(existListId)).willReturn(Optional.of(list));
+    given(userRepository.findById(eq(existUserId))).willReturn(Optional.of(user));
     given(cardRepository.save(any(Card.class))).willReturn(card);
 
-    card.setList(list);
-    card.setUser(user);
+    DTOContainer container = cardService.createCardInto(existBoardId, existListId, request);
 
-    DTOContainer container = cardService.createCardInto(1L, 1L, request);
-
-    assertThat(container).isNotNull();
+    assertThat(container.getResultType()).isEqualTo(RESULT_TYPE_CARD);
+    assertThat(container.getResult()).isNotNull();
+    assertThat(container.getLog()).isNotNull();
   }
 }
